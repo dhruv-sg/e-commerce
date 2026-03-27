d82518e25b0c1de431beec447e01560a32d2a4e7
dhruv gondaliya <dhruvsgondaliya@gmail.com>
2026-03-27 20:11:30 +0530

    email fix

--- utils/emailService.js
+++ utils/emailService.js
@@ -3,18 +3,24 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (to, subject, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // true for 465, false for 587
             auth: {
                 user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
+                pass: process.env.EMAIL_PASS,
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+            family: 4, // Force IPv4
         });
 
         const mailOptions = {
             from: `"YourStore" <${process.env.EMAIL_USER}>`,
             to,
             subject,
-            html
+            html,
         };
 
         const info = await transporter.sendMail(mailOptions);
